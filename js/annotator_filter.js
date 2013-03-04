30d4f05928efe7fb67651707ac1ebf0f1e7c3f91
Stefan Auditor <stefan.auditor@erdfisch.de>
2013-03-04 11:36:23 +0100

    reset filter plugin to defaults

--- js/annotator_filter.js
+++ js/annotator_filter.js
@@ -1,31 +1,7 @@
 (function ($) {
   Drupal.behaviors.annotatorFilter = {
     attach: function (context, settings) {
-      Drupal.Annotator.annotator('addPlugin', 'Filter', {
-        filters: [
-          {
-            label: Drupal.t('Quote'),
-            property: 'quote'
-          },
-          {
-            label: Drupal.t('Tag'),
-            property: 'tags',
-            isFiltered: function (input, tags) {
-              if (input && tags && tags.length) {
-                var keywords = input.split(/\s+/g);
-                for (var i = 0; i < keywords.length; i += 1) {
-                  for (var j = 0; j < tags.length; j += 1) {
-                    if (tags[j].indexOf(keywords[i]) !== -1) {
-                      return true;
-                    }
-                  }
-                }
-              }
-              return false;
-            }
-          }
-        ]
-      });
+      Drupal.Annotator.annotator('addPlugin', 'Filter');
     }
   };
 })(jQuery);
